68bdf908bb02188f97e28659b63306bbe5fd33ec
jose-guerrerog <joseguerrerog880@gmail.com>
2024-06-17 16:28:10 +1000

    update cards

--- src/app/components/ProjectCard.tsx
+++ src/app/components/ProjectCard.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState } from "react";
-import { Box, Typography } from "@mui/material";
+import { Box, Card, CardMedia, CardContent, Typography } from "@mui/material";
 import Link from "next/link";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import CodeIcon from "@mui/icons-material/Code";
@@ -24,77 +24,75 @@ const ProjectCard = ({
     setIsCardHovered(false);
   };
   return (
-    <Box component="section" id="projects">
-      <Box
-        position="relative"
-        component="div"
-        onMouseEnter={onMouseEnter}
-        onMouseLeave={onMouseLeave}
-      >
-        {isCardHovered && (
-          <Box
-            component="div"
-            sx={{
-              position: "absolute",
-              top: 0,
-              left: 0,
-              width: "100%",
-              height: "100%",
-              backgroundColor: "#000",
-              zIndex: 1,
-              opacity: 0.5,
-            }}
-          />
-        )}
-        <img
-          src={image}
-          width="100%"
-          height="auto"
-          alt="project-catd"
-          style={{
-            opacity: isCardHovered ? 0.8 : 1,
-            maxWidth: "100%",
-            display: "block",
+    <Box
+      position="relative"
+      component="div"
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+    >
+      {isCardHovered && (
+        <Box
+          component="div"
+          sx={{
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            backgroundColor: "#000",
+            zIndex: 1,
+            opacity: 0.5,
           }}
         />
-        <Link href={githubLink}>
-          <CodeIcon
-            sx={{
-              color: "#ADB7BE",
-              opacity: isCardHovered ? 1 : 0,
-              position: "absolute",
-              left: "35%",
-              top: "40%",
-              height: 40,
-              width: 40,
-              "&:hover": {
-                color: "#FFFFFF",
-              },
-              zIndex: 2,
-            }}
-          />
-        </Link>
-        <Link href={demoLink}>
-          <VisibilityIcon
-            sx={{
-              color: "#ADB7BE",
-              opacity: isCardHovered ? 1 : 0,
-              position: "absolute",
-              left: "55%",
-              top: "40%",
-              height: 40,
-              width: 40,
-              "&:hover": {
-                color: "#FFFFFF",
-              },
-              zIndex: 2,
-            }}
-          />
-        </Link>
-      </Box>
-      <Box display="flex" justifyContent="center">
-        <Typography fontWeight={700} color="textPrimary">{title}</Typography>
-      </Box>
+      )}
+      <Card>
+        <CardMedia
+          component="img"
+          height="194"
+          image={image}
+          alt="image-card"
+        />
+        <CardContent>
+          <Typography fontWeight={700} color="textPrimary">
+            {title}
+          </Typography>
+        </CardContent>
+      </Card>
+
+      <Link href={githubLink}>
+        <CodeIcon
+          sx={{
+            color: "#ADB7BE",
+            opacity: isCardHovered ? 1 : 0,
+            position: "absolute",
+            left: "35%",
+            top: "40%",
+            height: 40,
+            width: 40,
+            "&:hover": {
+              color: "#FFFFFF",
+            },
+            zIndex: 2,
+          }}
+        />
+      </Link>
+      <Link href={demoLink}>
+        <VisibilityIcon
+          sx={{
+            color: "#ADB7BE",
+            opacity: isCardHovered ? 1 : 0,
+            position: "absolute",
+            left: "55%",
+            top: "40%",
+            height: 40,
+            width: 40,
+            "&:hover": {
+              color: "#FFFFFF",
+            },
+            zIndex: 2,
+          }}
+        />
+      </Link>
     </Box>
   );
 };
