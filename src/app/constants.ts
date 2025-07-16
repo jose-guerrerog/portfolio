5bfa3944cb0de959bb82b46548f3047048528df4
jose-guerrerog <joseguerrerog880@gmail.com>
2025-07-16 11:07:58 +1000

    remove crypto portfolio

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -68,13 +68,13 @@ export const icons = [
 ];
 
 export const projects = [
-  {
-    id: 11,
-    title: "Crypto Portfolio",
-    image: "/images/11.png",
-    githubLink: "https://github.com/jose-guerrerog/frontend-crypto/",
-    demoLink: "https://frontend-crypto-nine.vercel.app/",
-  },
+  // {
+  //   id: 11,
+  //   title: "Crypto Portfolio",
+  //   image: "/images/11.png",
+  //   githubLink: "https://github.com/jose-guerrerog/frontend-crypto/",
+  //   demoLink: "https://frontend-crypto-nine.vercel.app/",
+  // },
   // {
   //   id: 10,
   //   title: "MovieMatch",
